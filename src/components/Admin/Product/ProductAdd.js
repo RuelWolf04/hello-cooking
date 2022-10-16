86383c62da3de07b065f7256da233aa793f6786e
deedeezero4 <daisycancio123456@gmail.com>
2022-10-16 23:09:58 +0800

    test

--- src/components/Admin/Product/ProductAdd.js
+++ src/components/Admin/Product/ProductAdd.js
@@ -6,20 +6,25 @@ import React, { useState } from 'react'
 
 
 const ProductAdd = () => {
-  const [productName, setProductName] = useState('');
-  const [productPrice, setProductPrice] = useState('');
-  const [productDescription, setProductDescription] = useState('');
+  const [productName, setProductName] = useState('')
+  const [productPrice, setProductPrice] = useState('')
+  const [productDescription, setProductDescription] = useState('')
+  const [productDirection, setProductDirection] = useState('')
+  const [productPhoto, setProductPhoto] = useState('')
 
-  const handleSubmitForm = (e) => {
-    e.preventDefault();
+  const handleSubmitForm = () => {
+    // e.preventDefault();
 
     let formData = new FormData();
     formData.append('productName', productName)
     formData.append('productPrice', productPrice)
     formData.append('productDescription', productDescription)
+    formData.append('productDirection', productDirection)
+    formData.append('productPhoto', productPhoto)
     formData.append('function', 'insert')
 
-    axios.post({
+    axios({
+      method: 'POST',
       url: 'http://localhost/WD11_PHP/HC_db/hc_product_db.php',
       data: formData,
       config: { headers: { 'Content - Type': 'multipart/form-data' } }
@@ -52,6 +57,11 @@ const ProductAdd = () => {
           <label htmlFor="productDescription">Product Desctiption:</label>
           <textarea name="productDescription" id="productDescription" cols="30" rows="10" value={productDescription} onChange={e => setProductDescription(e.target.value)}></textarea>
         </div>
+        <div className="inputs">
+          <label htmlFor="productDirection">Cooking Directions:</label>
+          <textarea name="productDirection" id="productDirection" cols="30" rows="10" value={productDirection} onChange={e => setProductDirection(e.target.value)}></textarea>
+        </div>
+        <input type="file" name="productPhoto" id="productPhoto" value={productPhoto} onChange={e => setProductPhoto(e.target.value)} />
         <input type="button" value="Submit" name="submit" onClick={handleSubmitForm} />
       </form>
 
